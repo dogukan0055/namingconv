5ec2f9849d642ab0ed5e159b78d2e0685d070c86
dogukan0055 <102733433+dogukan0055@users.noreply.github.com>
2023-07-12 17:05:10 +0300

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+
+ini_set('display_startup_errors', 1);
+
+error_reporting(E_ALL);
 // Retrieve form data
 $businessUnit = $_POST['business_unit'];
 $franchise = $_POST['franchise'];
